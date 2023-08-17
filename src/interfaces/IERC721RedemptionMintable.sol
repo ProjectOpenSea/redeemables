55324da10a99c5574dc3fca4361db6e781855c2a
Stephan Min <stephankmin@gmail.com>
2023-08-17 19:26:54 -0400

    forge fmt

--- src/interfaces/IERC721RedemptionMintable.sol
+++ src/interfaces/IERC721RedemptionMintable.sol
@@ -4,8 +4,5 @@ pragma solidity ^0.8.19;
 import {ConsiderationItem} from "seaport-types/src/lib/ConsiderationStructs.sol";
 
 interface IERC721RedemptionMintable {
-    function mintRedemption(
-        address to,
-        ConsiderationItem[] calldata spent
-    ) external returns (uint256 tokenId);
+    function mintRedemption(address to, ConsiderationItem[] calldata spent) external returns (uint256 tokenId);
 }
