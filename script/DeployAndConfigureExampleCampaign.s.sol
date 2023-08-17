71bb2e7992608646b131f75e19f36d18802ce4d9
Stephan Min <stephankmin@gmail.com>
2023-08-17 17:56:17 -0400

    update script to deploy redeemable and redemption tokens

--- script/DeployAndConfigureExampleCampaign.s.sol
+++ script/DeployAndConfigureExampleCampaign.s.sol
@@ -14,7 +14,8 @@ contract DeployAndConfigureExampleCampaign is Script {
     // Addresses: Seaport
     address seaport = 0x00000000000000ADc04C56Bf30aC9d3c0aAF14dC;
     address conduit = 0x1E0049783F008A0085193E00003D00cd54003c71;
-    bytes32 conduitKey = 0x0000007b02230091a7ed01230072f7006a004d60a8d4e71d599b8104250f0000;
+    bytes32 conduitKey =
+        0x0000007b02230091a7ed01230072f7006a004d60a8d4e71d599b8104250f0000;
 
     address constant _BURN_ADDRESS = 0x000000000000000000000000000000000000dEaD;
 
@@ -35,7 +36,7 @@ contract DeployAndConfigureExampleCampaign is Script {
         // Configure the campaign.
         OfferItem[] memory offer = new OfferItem[](1);
         offer[0] = OfferItem({
-            itemType: ItemType.ERC721,
+            itemType: ItemType.ERC721_WITH_CRITERIA,
             token: address(redemptionToken),
             identifierOrCriteria: 0,
             startAmount: 1,
@@ -44,7 +45,7 @@ contract DeployAndConfigureExampleCampaign is Script {
 
         ConsiderationItem[] memory consideration = new ConsiderationItem[](1);
         consideration[0] = ConsiderationItem({
-            itemType: ItemType.ERC721,
+            itemType: ItemType.ERC721_WITH_CRITERIA,
             token: address(redeemableToken),
             identifierOrCriteria: 0,
             startAmount: 1,
@@ -59,9 +60,12 @@ contract DeployAndConfigureExampleCampaign is Script {
             startTime: uint32(block.timestamp),
             endTime: uint32(block.timestamp + 1_000_000),
             maxCampaignRedemptions: 1_000,
-            manager: address(this)
+            manager: msg.sender
         });
-        offerer.updateCampaign(0, params, "");
+        offerer.createCampaign(
+            params,
+            "ipfs://QmdChMVnMSq4U6oVKhud7wUSEZGnwuMuTY5rUQx57Ayp6H"
+        );
 
         // Mint tokens 1 and 5 to redeem for tokens 1 and 5.
         redeemableToken.mint(msg.sender, 1);
