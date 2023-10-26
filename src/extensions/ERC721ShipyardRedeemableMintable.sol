deef60c2e0e8f122ba3db16db5621fc2413b0948
Stephan Min <stephankmin@gmail.com>
2023-10-26 18:42:48 -0400

    add src

--- src/extensions/ERC721ShipyardRedeemableMintable.sol
+++ src/extensions/ERC721ShipyardRedeemableMintable.sol
@@ -15,9 +15,12 @@ import {TraitRedemption} from "../lib/RedeemablesStructs.sol";
 contract ERC721ShipyardRedeemableMintable is ERC721ShipyardRedeemable, IRedemptionMintable {
     /// @dev Revert if the sender of mintRedemption is not this contract.
     error InvalidSender();
+    error InvalidTokenURIQuery();
 
     /// @dev The next token id to mint.
     uint256 _nextTokenId = 1;
+
+    mapping(uint256 => uint256) public tokenURINumbers;
 
     constructor(string memory name_, string memory symbol_) ERC721ShipyardRedeemable(name_, symbol_) {}
 
@@ -45,5 +48,41 @@ contract ERC721ShipyardRedeemableMintable is ERC721ShipyardRedeemable, IRedempti
     {
         return interfaceId == type(IRedemptionMintable).interfaceId
             || ERC721ShipyardRedeemable.supportsInterface(interfaceId);
+    }
+
+    /**
+     * @notice Hook to set tokenURINumber on mint.
+     */
+    function _beforeTokenTransfer(address from, address, /* to */ uint256 id) internal virtual override {
+        // Set tokenURINumbers on mint.
+        if (from == address(0)) {
+            // 60% chance of tokenURI 1
+            // 30% chance of tokenURI 2
+            // 10% chance of tokenURI 3
+
+            // block.difficulty returns PREVRANDAO on Ethereum post-merge
+            // NOTE: do not use this on other chains
+            uint256 randomness = (uint256(keccak256(abi.encode(block.difficulty))) % 100) + 1;
+
+            uint256 tokenURINumber = 1;
+            if (randomness >= 60 && randomness < 90) {
+                tokenURINumber = 2;
+            } else if (randomness >= 90) {
+                tokenURINumber = 3;
+            }
+
+            tokenURINumbers[id] = tokenURINumber;
+        }
+    }
+
+    /*
+     * @notice Overrides the `tokenURI()` function to return baseURI + 1, 2, or 3
+     */
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        if (!_exists(tokenId)) revert InvalidTokenURIQuery();
+
+        uint256 tokenURINumber = tokenURINumbers[tokenId];
+
+        return string(abi.encodePacked(baseURI, _toString(tokenURINumber)));
     }
 }
