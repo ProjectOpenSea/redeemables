e13d712358660c13ab8b6b444b1c799424e81b30
Ryan Ghods <ryan@ryanio.com>
2023-11-20 14:40:36 -0800

    fix test

--- test/ERC721ShipyardRedeemable.t.sol
+++ test/ERC721ShipyardRedeemable.t.sol
@@ -1,11 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {BaseRedeemablesTest} from "./utils/BaseRedeemablesTest.sol";
+import {Test} from "forge-std/Test.sol";
 import {ERC721} from "solady/src/tokens/ERC721.sol";
 import {ERC721ShipyardRedeemableOwnerMintable} from "../src/test/ERC721ShipyardRedeemableOwnerMintable.sol";
 
-contract TestERC721ShipyardRedeemable is BaseRedeemablesTest {
+contract TestERC721ShipyardRedeemable is Test {
+    // This Transfer event is different than the one in BaseOrderTest, since `id` is indexed.
+    // We don't inherit BaseRedeemablesTest to avoid this conflict.
+    // For more details see https://github.com/ethereum/solidity/issues/4168#issuecomment-1819912098
+    event Transfer(address indexed from, address indexed to, uint256 indexed id);
+
     function testBurn() public {
         uint256 tokenId = 1;
         ERC721ShipyardRedeemableOwnerMintable token = new ERC721ShipyardRedeemableOwnerMintable("Test", "TEST");
@@ -15,7 +20,7 @@ contract TestERC721ShipyardRedeemable is BaseRedeemablesTest {
         vm.expectRevert(ERC721.NotOwnerNorApproved.selector);
         token.burn(tokenId);
 
-        vm.expectEmit(true, true, true, true);
+        vm.expectEmit(true, true, false, false);
         emit Transfer(fred, address(0), tokenId);
         vm.prank(fred);
         token.burn(tokenId);
@@ -30,8 +35,12 @@ contract TestERC721ShipyardRedeemable is BaseRedeemablesTest {
         vm.prank(fred);
         token.setApprovalForAll(address(this), true);
 
-        vm.expectEmit(true, true, true, true);
+        vm.expectEmit(true, true, false, false);
         emit Transfer(fred, address(0), tokenId + 1);
         token.burn(tokenId + 1);
+    }
+
+    function _mintToken(address token, uint256 tokenId, address recipient) internal {
+        ERC721ShipyardRedeemableOwnerMintable(address(token)).mint(recipient, tokenId);
     }
 }
